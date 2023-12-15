f2747f9818fe8b611c37498233fa188e0d95a2c8
khwong-c64 <kin.hin.wong.c@gmail.com>
2023-12-15 16:16:02 +0000

    SV Constant expression and prelim test

--- magia/util.py
+++ magia/util.py
@@ -8,4 +8,4 @@ def sv_constant(value: Union[int, bytes], width: int, signed: bool = False) -> s
         value = value.to_bytes(byte_cnt, byteorder="big", signed=signed)
     byte_mask = (2 ** width - 1).to_bytes(byte_cnt, byteorder="big")
     value = bytes([x & y for x, y in zip(value, byte_mask)])
-    return f"{width}'h{value.hex()}"
+    return f"{width}'h{value.hex()[-(ceil(width / 4)):].upper()}"
