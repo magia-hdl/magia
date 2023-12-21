1699ad089a946f72ac6972878f388d947e80007b
khwong-c64 <kin.hin.wong.c@gmail.com>
2023-12-21 20:11:38 +0000

    AXI4 Stream Bundle

--- magia/stl/bundles.py
+++ magia/stl/bundles.py
@@ -35,3 +35,53 @@ class StdIO:
         return cls.decoupled_multi(name, {name: (width, signed)}, sep)
 
 
+
+
+
+class AXI4Stream:
+    """
+    Define a AXI4 Stream interface factory.
+    The factory specializes parameters for the AXI4 Stream interface.
+    Using `master` or `slave` will return an IOBundle with the appropriate signal names and directions
+    """
+
+    def __init__(
+            self,
+            tdata_width: int = 32,
+            tid_width: int = 0,
+            tdest_width: int = 0,
+            tuser_width: int = 0,
+            **kwargs
+    ):
+        self.tdata_width = tdata_width
+        self.tid_width = tid_width
+        self.tdest_width = tdest_width
+        self.tuser_width = tuser_width
+
+    def _main_interface(self, **kwargs) -> IOBundle:
+
+        new_bundle = IOBundle()
+        new_bundle += Input("aclk", 1)
+        new_bundle += Input("aresetn", 1)
+        data_spec = {
+            "tdata": self.tdata_width,
+            "tstrb": self.tdata_width // 8,
+            "tkeep": self.tdata_width // 8,
+            "tlast": 1,
+        }
+        if self.tid_width:
+            data_spec["tid"] = self.tid_width
+        if self.tdest_width:
+            data_spec["tdest"] = self.tdest_width
+        if self.tuser_width:
+            data_spec["tuser"] = self.tuser_width
+        new_bundle += StdIO.decoupled_multi("t", data_spec, sep="")
+
+        return new_bundle.with_name(prefix="axis_")
+
+    def master(self, **kwargs) -> IOBundle:
+        return self._main_interface(**kwargs)
+
+    def slave(self, **kwargs) -> IOBundle:
+        slave_bundle = self._main_interface(**kwargs)
+        return slave_bundle.flip(ignore=["aclk, aresetn"])
