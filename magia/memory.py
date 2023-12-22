ae661ea3fd099b98596482e9b6472dc69abec441
khwong-c64 <kin.hin.wong.c@gmail.com>
2023-12-22 23:13:56 +0000

    Placeholder for memory

---
+++ magia/memory.py
@@ -0,0 +1,10 @@
+import string
+from dataclasses import dataclass
+from itertools import count
+from typing import Optional
+
+from .bundle import IOBundle
+from .core import Input, Output
+from .module import Blackbox, Module
+
+
