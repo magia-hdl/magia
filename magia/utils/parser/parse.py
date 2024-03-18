a5abbf646167af0416e2002e7b029c76b39311ff
khwong-c64 <kin.hin.wong.c@gmail.com>
2024-03-18 20:17:11 +0000

    Allow multiple entering point

--- magia/utils/parser/parse.py
+++ magia/utils/parser/parse.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from antlr4 import CommonTokenStream, InputStream
 from antlr4.tree.Tree import Tree
 
@@ -5,9 +7,16 @@ from .SystemVerilogLexer import SystemVerilogLexer
 from .SystemVerilogParser import SystemVerilogParser
 
 
-def parse_sv_code(sv_code: str, **kwargs) -> Tree:
+def parse_sv_code(
+        sv_code: str,
+        parse_as: Literal["source_text", "concurrent_assertion_item"] = "source_text",
+        **kwargs
+) -> Tree:
     lexer = InputStream(sv_code)
     lexer_grammar = SystemVerilogLexer(lexer)
     stream = CommonTokenStream(lexer_grammar)
     parser = SystemVerilogParser(stream)
-    return parser.source_text()
+    return {
+        "source_text": parser.source_text,
+        "concurrent_assertion_item": parser.concurrent_assertion_item,
+    }[parse_as]()
