b8974cfd5c3f6514aab3633a14724e476646506a
khwong-c64 <kin.hin.wong.c@gmail.com>
2024-03-25 00:46:12 +0000

    FSM Generator with Logic decoupled

--- magia/gen/fsm.py
+++ magia/gen/fsm.py
@@ -1,156 +1,260 @@
-from collections import UserDict
+import copy
 from dataclasses import dataclass, field
 from typing import Optional
 
-from magia import Constant, Register, Signal
+from magia import Constant, Signal
 
 
-class FSM:
+class FSMLogic:
     @dataclass
     class State:
         name: str
-        code: int
+        code: Optional[int]
         signal: Constant = field(repr=False)
+        desc: Optional[str] = field(default=None, repr=False)
 
     @dataclass
     class Transition:
         next: str
         cond: Optional[Signal] = None
-        desc: str = ""
-
-        def __post_init__(self):
-            if self.cond is not None and self.cond.width != 1:
-                raise ValueError("All transition condition must have only 1 bit")
 
     fsm_id = 0
 
     def __init__(
             self,
             clk: Signal,
-            states: dict[str, int],
-            transitions: dict[str, list],
             reset_state: str,
             reset: Optional[Signal] = None,
             async_reset: Optional[Signal] = None,
-            description: Optional[str] = None,
             name: Optional[str] = None,
+            **kwargs,
     ):
         if async_reset is None and reset is None:
             raise ValueError("Specify at least one of the reset/async_reset signal")
-        if reset_state not in states:
-            raise ValueError(f"Reset state is not defined in states {reset_state=}")
 
-        self.name = f"{name if name else self.fsm_id}"
-        self.name_signal = f"fsm_{self.name}"
+        self.states: dict[str, FSMLogic.State] = {}
+        self.transitions: dict[str, list[FSMLogic.Transition]] = {}
+        self.finalized = False
+
+        self.fsm_name = f"{name if name else self.fsm_id}"
         self.fsm_id += 1
-        self.description = description
+        self.gen_id = 0
 
-        self._states, state_width = self._init_states(states)
-
-        _update_state = Signal(1)
-        self._state = Register(
-            name=f"{self.name_signal}_state",
-            width=state_width,
-            clk=clk,
-            enable=_update_state,
-            reset=reset, async_reset=async_reset,
-            reset_value=self.states[reset_state].code,
-            async_reset_value=self.states[reset_state].code,
-        )
-
-        self.transitions = {
-            state: [
-                self.Transition(**trans) if not isinstance(trans, self.Transition) else trans
-                for trans in trans_list
-            ]
-            for state, trans_list in transitions.items()
+        self.state_width = 0
+        self.reset_state = reset_state
+        self._clocking = {
+            "clk": clk,
+            "reset": reset,
+            "async_reset": async_reset,
         }
-        self._validate_transitions()
-
-        next_state, update_state = self._gen_fsm_signals()
-        self._state <<= next_state
-        _update_state <<= update_state
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def states(self):
-        return self._states
 
     @property
     def width(self):
-        return self.state.width
+        if not self.finalized:
+            raise ValueError("FSM is not finalized")
+        return self.state_width
 
-    def case(self, default=None, **cases):
-        cases = {
-            self.states[state].code: signal
-            for state, signal in cases.items()
-        }
-        return self.state.case(cases, default)
+    def add_states(self, **states):
+        for name, code in states.items():
+            self.add_state(name, code)
+        return self
 
-    def _one_state_signals(self, transitions) -> tuple[Signal, Signal]:
+    def add_state(self, name: str, code: Optional[int] = None):
+        if self.finalized:
+            raise ValueError("FSM is already finalized")
+
+        if name in self.states:
+            raise ValueError(f"State {name} already exists")
+        if code is not None:
+            existing_code = {state.code for state in self.states.values() if state.code is not None}
+            if code in existing_code:
+                raise ValueError(f"Code {code} already exists")
+
+        self.states[name] = self.State(name, code, None)
+        self.transitions[name] = []
+
+        return self
+
+    def add_transitions(self, *transitions: tuple[str, str, Optional[Signal]]):
+        for src, dst, cond in transitions:
+            self.add_transition(src, dst, cond)
+        return self
+
+    def _check_transition(self, src: str, dst: str, cond: Optional[Signal] = None):
+        if self.finalized:
+            raise ValueError("FSM is already finalized")
+        if cond is not None and cond.width != 1:
+            raise ValueError("All transition condition must have only 1 bit")
+        if src not in self.states:
+            raise ValueError(f"State {src} is not defined")
+        if dst not in self.states:
+            raise ValueError(f"State {dst} is not defined")
+
+    def add_transition(self, src: str, dst: str, cond: Optional[Signal] = None):
+        self._check_transition(src, dst, cond)
+        self.transitions[src].append(self.Transition(dst, cond))
+        return self
+
+    def push_transition(self, src: str, dst: str, cond: Optional[Signal] = None):
+        self._check_transition(src, dst, cond)
+        self.transitions[src].insert(0, self.Transition(dst, cond))
+        return self
+
+    def finalize(self):
+        if self.finalized:
+            raise ValueError("FSM is already finalized")
+        self.finalized = True
+
+        # Check if reset state exists
+        if self.reset_state not in self.states:
+            raise ValueError(f"Reset state {self.reset_state} is not defined")
+
+        # Fill in missing codes
+        existing_code = {state.code for state in self.states.values() if state.code is not None}
+        for state in self.states.values():
+            if state.code is None:
+                if len(existing_code) == 0:
+                    state.code = 0
+                else:
+                    max_code = max(existing_code) if len(existing_code) else 0
+                    free_codes = [c for c in range(max_code + 1) if c not in existing_code]
+                    state.code = min(free_codes) if len(free_codes) else (max_code + 1)
+                existing_code.add(state.code)
+
+        # Generate states Constant signals
+        self.state_width = max(state.code for state in self.states.values()).bit_length()
+        for state_name, state in self.states.items():
+            state.signal = Constant(
+                state.code, width=self.state_width,
+                name=f"const_{self.fsm_name}_state_{state_name}",
+            )
+
+        return self
+
+    def _fsm_logic_one_state(self, transitions, prev_state) -> Signal:
         next_state = Constant(0, self.state_width)
-        update_state = Constant(0, 1)
         prev_cond = Constant(0, 1)
 
         for i, trans in enumerate(transitions):
             if trans.cond:
                 next_state |= self.states[trans.next].signal.when((~prev_cond) & trans.cond, else_=0)
-                update_state |= trans.cond
                 prev_cond |= trans.cond
             else:
                 if i != len(transitions) - 1:
                     raise ValueError("Unconditional transition can only be the last entry")
                 next_state |= self.states[trans.next].signal.when(~prev_cond, else_=0)
-                update_state = Constant(1, 1)
+                prev_cond = Constant(1, 1)
+        next_state |= prev_state.when(~prev_cond, else_=0)
 
-        return next_state, update_state
+        return next_state
 
-    def _gen_fsm_signals(self) -> tuple[Signal, Signal]:
-        next_and_update = {
-            state: self._one_state_signals(transitions)  # next_state, update_state
+    def _gen_fsm_logic(self, prev_state: Signal) -> Signal:
+        new_state_logics = {
+            state: self._fsm_logic_one_state(transitions, prev_state)
             for state, transitions in self.transitions.items()
         }
-        next_state = self.state.case(
+        new_state = prev_state.case(
             {
-                self.states[state].code: next_up[0]
-                for state, next_up in next_and_update.items()
+                self.states[state].code: next_state
+                for state, next_state in new_state_logics.items()
             },
-            default=self.state
+            default=prev_state,
         )
-        update_state = self.state.case(
-            {
-                self.states[state].code: next_up[1]
-                for state, next_up in next_and_update.items()
-            },
-            default=0
+        return new_state
+
+    def generate(self) -> tuple[Signal, Signal]:  # (input, state register)
+        if not self.finalized:
+            self.finalize()
+
+        register_name = f"fsm_{self.fsm_name}_{self.gen_id}_state"
+        input_name = f"fsm_{self.fsm_name}_{self.gen_id}_input"
+        self.gen_id += 1
+
+        reset_value = self.states[self.reset_state].code
+
+        state_input = Signal(self.state_width, name=input_name)
+        state_register = self._gen_fsm_logic(state_input).reg(
+            name=register_name,
+            reset_value=reset_value,
+            async_reset_value=reset_value,
+            **self._clocking,
         )
-        return next_state, update_state
 
-    def _validate_transitions(self):
-        for src_state, trans_lists in self.transitions.items():
-            if src_state not in self.states:
-                raise ValueError("State in transitions is undefined: {src_state}")
-            for transition in trans_lists:
-                if transition.next not in self.states:
-                    raise ValueError("State in transitions is undefined: {transition.next_state}")
+        return state_input, state_register
 
-    def _init_states(self, states: dict[str, int]) -> tuple[dict[str, State], int]:
-        max_code = max(states.values())
-        state_width = max_code.bit_length()
-        states_in_fsm = UserDict(**{
-            name: self.State(
-                name=name,
-                code=code,
-                signal=Constant(
-                    name=f"const_{self.name_signal}_state_{name}",
-                    value=code, width=state_width,
-                )
-            )
-            for name, code in states.items()
-        })
-        for name in states:
-            setattr(states_in_fsm, name, states_in_fsm[name])
-        return states_in_fsm, state_width
+    def copy(self, name: Optional[str] = None, **kwargs) -> "FSMLogic":
+        new_fsm = FSMLogic(
+            **kwargs,
+            reset_state=self.reset_state,
+            name=name,
+            **self._clocking,
+        )
+        new_fsm.states = copy.deepcopy(self.states)
+        new_fsm.transitions = {
+            state: [(src, dst, cond) for src, dst, cond in transitions]
+            for state, transitions in self.transitions.items()
+        }
+        return new_fsm
+
+
+class FSM:
+    def __init__(
+            self,
+            clk: Signal,
+            reset_state: str,
+            reset: Optional[Signal] = None,
+            async_reset: Optional[Signal] = None,
+            name: Optional[str] = None,
+            fsm_stage: Optional[FSMLogic] = None,
+            **kwargs,
+    ):
+        self.fsm_logic = FSMLogic(
+            clk, reset_state, reset, async_reset, name, **kwargs
+        ) if fsm_stage is None else fsm_stage
+        self.state: Optional[Signal] = None
+
+    @property
+    def states(self):
+        return self.fsm_logic.states
+
+    @property
+    def transitions(self):
+        return self.fsm_logic.transitions
+
+    @property
+    def width(self):
+        if not self.fsm_logic.finalized:
+            raise ValueError("FSM is not finalized")
+        return self.fsm_logic.state_width
+
+    def add_states(self, **states):
+        self.fsm_logic.add_states(**states)
+        return self
+
+    def add_state(self, name: str, code: Optional[int] = None):
+        self.fsm_logic.add_state(name, code)
+        return self
+
+    def add_transitions(self, *transitions: tuple[str, str, Optional[Signal]]):
+        self.fsm_logic.add_transitions(*transitions)
+        return self
+
+    def add_transition(self, src: str, dst: str, cond: Optional[Signal] = None):
+        self.fsm_logic.add_transition(src, dst, cond)
+        return self
+
+    def push_transition(self, src: str, dst: str, cond: Optional[Signal] = None):
+        self.fsm_logic.push_transition(src, dst, cond)
+        return self
+
+    def finalize(self):
+        self.fsm_logic.finalize()
+        return self
+
+    def generate(self) -> Signal:
+        if self.state:
+            raise ValueError("FSM has already been generated")
+
+        prev, self.state = self.fsm_logic.generate()
+        prev <<= self.state
+        return self.state
