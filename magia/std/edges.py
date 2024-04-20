c710e6282c89bfb728ff442e41a0f06a884e923e
khwong-c64 <kin.hin.wong.c@gmail.com>
2024-04-20 20:42:50 +0100

    prev signal is now un-reset. Test added with first cycle skipped.

--- magia/std/edges.py
+++ magia/std/edges.py
@@ -1,12 +1,13 @@
-from typing import Optional
+"""Edge detectors."""
+from typing import Literal
 
 from magia import Signal
 
 
 def edge_detector(
         signal: Signal, clk: Signal,
-        reset: Optional[Signal] = None, async_reset: Optional[Signal] = None,
-        edge: str = "rising", registered: bool = True,
+        reset: None | Signal = None, async_reset: None | Signal = None,
+        edge: Literal["rising", "falling", "both"] = "rising", registered: bool = True,
 ) -> Signal:
     """Detects the given edge of the input signal.
 
@@ -14,23 +15,23 @@ def edge_detector(
     :param clk: The clock signal.
     :param reset: The reset signal.
     :param async_reset: The async reset signal.
-    :param edge: The edge to detect, "rising", "falling" or "both". Default is "rising".
+    :param edge: The edge to detect. Default is "rising".
     :param registered: Register the output signal. Default is True.
 
     :return: The detected edge signal.
     """
-    if reset is None and async_reset is None:
+    if registered and reset is None and async_reset is None:
         raise ValueError("reset or async_reset must be provided")
     if edge not in ("rising", "falling", "both"):
         raise ValueError('edge must be "rising", "falling" or "both"')
-    if len(signal) != 1:
+    if signal.width != 1:
         raise ValueError("signal must be a single bit")
     resets = {
         "reset": reset, "async_reset": async_reset,
         "reset_value": 0, "async_reset_value": 0
     }
 
-    prev = signal.reg(clk, **resets)
+    prev = signal.reg(clk)
     edge_detected = {
         "rising": signal & ~prev,
         "falling": ~signal & prev,
--- tests/std/test_edges.py
+++ tests/std/test_edges.py
@@ -1,15 +1,81 @@
-from magia import Elaborator, Input, Module, Output
+import random
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge
+from magia_flow.simulation.general import Simulator
+
+from magia import Input, Module, Output
 from magia.std.edges import edge_detector
+from tests.helper import simulate
+
+sim_module_and_path = {
+    "test_module": [Simulator.current_package()],
+    "python_search_path": [Simulator.current_dir()],
+}
 
 
-class ALlEdges(Module):
+@cocotb.test()
+async def edge_detection(dut):
+    """Check the functionality of edge detection."""
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+
+    await FallingEdge(dut.clk)
+
+    dut.reset.value = 1
+    dut.rst_n.value = 0
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    dut.rst_n.value = 1
+
+    for i in range(1000):
+        # Drive signal and setup expectation
+        cur_event, prev_event = random.randint(0, 1), (cur_event if i != 0 else None)
+        dut.event_in.value = cur_event
+        expectation = (
+            1 if cur_event == 1 and prev_event == 0 else 0,
+            1 if cur_event == 0 and prev_event == 1 else 0,
+            1 if cur_event != prev_event else 0,
+        ) if prev_event is not None else None
+
+        await cocotb.clock.Timer(1)
+        if expectation is not None:
+            exp_rising_unreg, exp_falling_unreg, exp_both_unreg = expectation
+            assert dut.rising_unreg.value == exp_rising_unreg or exp_rising_unreg is None, \
+                f"Rising edge failed at {cur_event}, {prev_event}"
+            assert dut.falling_unreg.value == exp_falling_unreg or exp_falling_unreg is None, \
+                f"Falling edge failed at {cur_event}, {prev_event}"
+            assert dut.both_unreg.value == exp_both_unreg or exp_both_unreg is None, \
+                f"Both edge failed at {cur_event}, {prev_event}"
+
+        # Wait for the next clock edge
+        await FallingEdge(dut.clk)
+
+        if expectation is not None:
+            exp_rising, exp_falling, exp_both = expectation
+            assert dut.rising.value == exp_rising or exp_rising is None, \
+                f"Rising edge failed at {cur_event}, {prev_event}"
+            assert dut.falling.value == exp_falling or exp_falling is None, \
+                f"Falling edge failed at {cur_event}, {prev_event}"
+            assert dut.both.value == exp_both or exp_both is None, \
+                f"Both edge failed at {cur_event}, {prev_event}"
+            assert dut.rising_async.value == exp_rising or exp_rising is None, \
+                f"Rising edge failed at {cur_event}, {prev_event}"
+            assert dut.falling_async.value == exp_falling or exp_falling is None, \
+                f"Falling edge failed at {cur_event}, {prev_event}"
+            assert dut.both_async.value == exp_both or exp_both is None, \
+                f"Both edge failed at {cur_event}, {prev_event}"
+
+
+class AllEdges(Module):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.io += [
             Input("clk", 1),
             Input("reset", 1),
             Input("rst_n", 1),
-            Input("event", 1),
+            Input("event_in", 1),
             Output("rising", 1),
             Output("falling", 1),
             Output("both", 1),
@@ -19,42 +85,41 @@ class ALlEdges(Module):
             Output("rising_async", 1),
             Output("falling_async", 1),
             Output("both_async", 1),
-            Output("rising_async_unreg", 1),
-            Output("falling_async_unreg", 1),
-            Output("both_async_unreg", 1),
         ]
 
         sync_clock = {
+            "signal": self.io.event_in,
             "clk": self.io.clk,
             "reset": self.io.reset,
         }
-        sync_clock_unreg = {
-            **sync_clock,
-            "registered": False,
-        }
         async_clock = {
+            "signal": self.io.event_in,
             "clk": self.io.clk,
             "async_reset": self.io.rst_n,
         }
-        async_clock_unreg = {
-            **async_clock,
+        unregistered = {
+            "clk": self.io.clk,
+            "signal": self.io.event_in,
+            "reset": self.io.reset,
             "registered": False,
         }
 
-        self.io.rising <<= edge_detector(self.io.event, edge="rising", **sync_clock)
-        self.io.falling <<= edge_detector(self.io.event, edge="falling", **sync_clock)
-        self.io.both <<= edge_detector(self.io.event, edge="both", **sync_clock)
-        self.io.rising_unreg <<= edge_detector(self.io.event, edge="rising", **sync_clock_unreg)
-        self.io.falling_unreg <<= edge_detector(self.io.event, edge="falling", **sync_clock_unreg)
-        self.io.both_unreg <<= edge_detector(self.io.event, edge="both", **sync_clock_unreg)
+        self.io.rising <<= edge_detector(edge="rising", **sync_clock)
+        self.io.falling <<= edge_detector(edge="falling", **sync_clock)
+        self.io.both <<= edge_detector(edge="both", **sync_clock)
 
-        self.io.rising_async <<= edge_detector(self.io.event, edge="rising", **async_clock)
-        self.io.falling_async <<= edge_detector(self.io.event, edge="falling", **async_clock)
-        self.io.both_async <<= edge_detector(self.io.event, edge="both", **async_clock)
-        self.io.rising_async_unreg <<= edge_detector(self.io.event, edge="rising", **async_clock_unreg)
-        self.io.falling_async_unreg <<= edge_detector(self.io.event, edge="falling", **async_clock_unreg)
-        self.io.both_async_unreg <<= edge_detector(self.io.event, edge="both", **async_clock_unreg)
+        self.io.rising_async <<= edge_detector(edge="rising", **async_clock)
+        self.io.falling_async <<= edge_detector(edge="falling", **async_clock)
+        self.io.both_async <<= edge_detector(edge="both", **async_clock)
+
+        self.io.rising_unreg <<= edge_detector(edge="rising", **unregistered)
+        self.io.falling_unreg <<= edge_detector(edge="falling", **unregistered)
+        self.io.both_unreg <<= edge_detector(edge="both", **unregistered)
 
 
-def test_edge_detector_smoke():
-    Elaborator.to_string(ALlEdges(name="Top"))
+def test_edge_detector():
+    all_edges = AllEdges(name="TopModule")
+    simulate(
+        "TopModule", all_edges, testcase="edge_detection",
+        **sim_module_and_path,
+    )
