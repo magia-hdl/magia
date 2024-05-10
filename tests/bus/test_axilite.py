c2302f0116ec40dcc66cddcf72c121febc2c3ca7
khwong-c64 <kin.hin.wong.c@gmail.com>
2024-05-10 23:55:36 +0100

    Fix Test

--- tests/bus/test_axilite.py
+++ tests/bus/test_axilite.py
@@ -1,5 +1,5 @@
+from magia import Input, Output
 from magia.bus.axilite import axi4lite
-from magia.data_struct import SignalType
 
 
 class TestAXI4Lite:
@@ -8,32 +8,32 @@ class TestAXI4Lite:
     def test_master_direction(self):
         master_ports = self.spec.master_ports()
         for ports, direction in [
-            (("aclk", "aresetn",), SignalType.INPUT),
-            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), SignalType.INPUT),
+            (("aclk", "aresetn",), Input),
+            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), Input),
             (("awaddr", "awprot", "awvalid", "araddr", "arprot", "arvalid", "rready", "wdata", "wstrb", "wvalid",
-              "bready",), SignalType.OUTPUT),
+              "bready",), Output),
         ]:
             for port in ports:
-                assert master_ports[port].type == direction
+                assert isinstance(master_ports[port], direction)
 
     def test_slave_direction(self):
         slave_ports = self.spec.slave_ports()
         for ports, direction in [
-            (("aclk", "aresetn",), SignalType.INPUT),
-            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), SignalType.OUTPUT),
+            (("aclk", "aresetn",), Input),
+            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), Output),
             (("awaddr", "awprot", "awvalid", "araddr", "arprot", "arvalid", "rready", "wdata", "wstrb", "wvalid",
-              "bready",), SignalType.INPUT),
+              "bready",), Input),
         ]:
             for port in ports:
-                assert slave_ports[port].type == direction
+                assert isinstance(slave_ports[port], direction)
 
     def test_monitor_directions(self):
         monitor = self.spec.monitor_ports()
         for ports, direction in [
-            (("aclk", "aresetn",), SignalType.INPUT),
-            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), SignalType.INPUT),
+            (("aclk", "aresetn",), Input),
+            (("awready", "arready", "rdata", "rresp", "rvalid", "wready", "bresp", "bvalid",), Input),
             (("awaddr", "awprot", "awvalid", "araddr", "arprot", "arvalid", "rready", "wdata", "wstrb", "wvalid",
-              "bready",), SignalType.INPUT),
+              "bready",), Input),
         ]:
             for port in ports:
-                assert monitor[port].type == direction
+                assert isinstance(monitor[port], direction)
